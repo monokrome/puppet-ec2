b30ce62282025c5adf2755581f7ca6043ad25415
Brandon R. Stoner <bstoner@yrsi.com>
2011-08-02 21:53:41 +0000

    Modified to install the EC2 repository and ec2-api-tools.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,18 @@
 class ec2 {
+	include apt
+
+	apt::add::repository {
+		"awstools":
+			identifier => "ppa:awstools-dev/awstools"
+	}
+
+	package {
+		"ec2-api-tools":
+			ensure => latest,
+			require => [
+				Exec["apt::add::repository::awstools"],
+				Exec["apt-update"],
+			]
+	}
 }
 
